d52dcdca8a1b63ae6f9879d79c3d99dd1441fb58
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-09-21 10:06:10 +0700

    done responsive

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,18 +1,35 @@
-import Button from "components/Button";
 import React, { useState, useEffect, useRef } from "react";
+import { useMediaQuery } from "react-responsive";
+
+import Button from "components/Button";
 import Baloon from "./Baloon";
+
+import facebookIcon from 'assets/images/icon-facebook.svg'
+import twitterIcon from 'assets/images/icon-twitter.svg'
+import pinterestIcon from 'assets/images/icon-pinterest.svg'
 
 let useClickOutside = (handler) => {
   let domNode = useRef();
 
   useEffect(() => {
     const ballon = document.querySelector(".balloon");
+    const ballonContent = document.querySelector(".balloon__content");
+    const ballonContentShare = document.querySelector(".balloon__content__share");
+    const listBox = document.querySelector(".listBox");
 
     let maybeHandler = (event) => {
       console.log(event.target)
 
 
-      if (!domNode.current.contains(event.target) && event.target !== ballon && !event.target.classList.contains("share-link")) {
+      if (
+        !domNode.current.contains(event.target) &&
+        event.target !== ballonContentShare &&
+        event.target !== listBox &&
+        event.target !== ballonContent &&
+        event.target !== ballon &&
+        !event.target.classList.contains("share-link") &&
+        !event.target.classList.contains("icon-link")
+      ) {
         handler();
       }
     };
@@ -30,6 +47,9 @@ export default function Toggle() {
   const [isButtonY, setIsButtonY] = useState(null);
   const [isButtonX, setIsButtonX] = useState(null);
 
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 580px)' })
+  const isBigScreen = useMediaQuery({ query: '(min-width: 580px)' })
+
   const handleToggle = () => {
     const elem = document.querySelector('.btn');
     setIsButtonY(elem.getBoundingClientRect().y)
@@ -45,28 +65,58 @@ export default function Toggle() {
 
   return (
     <div ref={domNode} className="toggle-social-media">
-      <Button className="btn bg-light" isPrimary onClick={handleToggle} aria-expanded={isOpen ? "true" : "false"} aria-controls="notifications-label"></Button>
+      <Button className={`btn bg-light ${isOpen ? "active" : ""}`} isPrimary onClick={handleToggle} aria-expanded={isOpen ? "true" : "false"} aria-controls="notifications-label"></Button>
+      {isTabletOrMobile &&
+         <div className={`balloon ${isOpen ? "active" : ""}`} style={{ top: -17 , left: -258 }} id="notifications-label">
+          <div className="balloon__content">
+            <span className="balloon__content__share">Share</span>
+            <ul role="listbox">
+              <li>
+                <a className="share-link" href="#test">
+                  <img className="icon-link" src={facebookIcon} alt="" />
+                </a>
+              </li>
+              <li>
+                <a className="share-link" href="#test">
+                  <img className="icon-link" src={twitterIcon} alt="" />
+                </a>
+              </li>
+              <li>
+                <a className="share-link" href="#test">
+                  <img className="icon-link" src={pinterestIcon} alt="" />
+                </a>
+              </li>
+            </ul>
+                <Button className={`btn btn-click bg-light ${isOpen ? "active" : ""}`} isPrimary onClick={handleToggle} aria-expanded={isOpen ? "true" : "false"} aria-controls="notifications-label"></Button>
+          </div>
+        </div>
+      }
+      {isBigScreen &&
       <Baloon>
-        <div className={`balloon ${isOpen ? "active" : ""}`} style={{ top: isButtonY - 190, left: isButtonX - 95 }} id="notifications-label" >
-          <ul role="listbox">
-            <li>
-              <a className="share-link" href="#test">
-                List 1
-              </a>
-            </li>
-            <li>
-              <a className="share-link" href="#test">
-                List 2
-              </a>
-            </li>
-            <li>
-              <a className="share-link" href="#test">
-                List 3
-              </a>
-            </li>
-          </ul>
+        <div className={`balloon ${isOpen ? "active" : ""}`} style={{ top: isButtonY - 75, left: isButtonX - 95 }} id="notifications-label">
+          <div className="balloon__content">
+            <span className="balloon__content__share">Share</span>
+            <ul role="listbox">
+              <li>
+                <a className="share-link" href="#test">
+                  <img className="icon-link" src={facebookIcon} alt="" />
+                </a>
+              </li>
+              <li>
+                <a className="share-link" href="#test">
+                  <img className="icon-link" src={twitterIcon} alt="" />
+                </a>
+              </li>
+              <li>
+                <a className="share-link" href="#test">
+                  <img className="icon-link" src={pinterestIcon} alt="" />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </Baloon>
+      }
     </div>
   );
 }
